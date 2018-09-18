2496fc072b43518fa80cf024a9c9b6d4cb440399
cottagefarmer <patrick@runthisproject.com>
2018-09-18 18:16:05 -0400

    test agai

--- production-application.tf
+++ production-application.tf
@@ -1,11 +1,7 @@
 provider "aws" {
   region                  = "eu-west-1"
   profile                 = "terraform-user"
-<<<<<<< HEAD
 }test 4
-=======
-} test
->>>>>>> d067e2b85efa3feea580a3ace26fa8779757472c
 
 variable "VPCID" {
   default = "vpc-0ae93dba1685fca8c"
